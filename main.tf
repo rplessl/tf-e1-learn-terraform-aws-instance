0888497ee535dc95f1e3859ddd2e694ccb1e3414
Roman Plessl <roman.plessl@prunux.ch>
2022-08-27 02:01:12 +0200

    updated info

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-08a9dc8f6d1c4f806"
+  ami           = "ami-07702eb3b2ef420a9"
   instance_type = "t2.micro"
 
   tags = {
